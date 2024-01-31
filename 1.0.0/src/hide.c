b48a77e3d895a3882fdebf2acff0a4c03a7e6689
missn0body <hulk128t@gmail.com>
2024-01-30 19:15:58 -0600

    Finalized version 1.0.0

--- 1.0.0/src/hide.c
+++ 1.0.0/src/hide.c
@@ -26,12 +26,11 @@
 const char *VERSION = "1.0.0";
 
 #define UNHIDE	(1 << 5)
-#define HEXMODE (1 << 6)
 
 void showUsage(void)
 {
-	printf("hide (%s): a simple blowfish scramble schtick by anson.\n", VERSION);
-        puts("Usage: hide -a (--ansi) -u (--unhide) -x (--hex) -h (--help)\n");
+	printf("hide (%s): a simple Blowfish scramble schtick by anson.\n", VERSION);
+        puts("Usage: hide -a (--ansi) -u (--unhide) -h (--help)\n");
         puts("Be warned: the only cipher mode supported as of now is EBC.");
 	puts("Use this application with the right data for the right purpose.");
 }
@@ -39,49 +38,6 @@ void showUsage(void)
 int main(int argc, char *argv[])
 {
 	_Assert(argc >= 2 || argv[1] != nullptr, "too few arguments. try \"--help\"");
-
-	/*
-	// BLOWFISH ENCRYPT, DECRYPT, AND ZERO TESTS
-
-	u32 L = 1, R = 2;
-  	BLOWFISH_CTX *ctx = malloc(sizeof(BLOWFISH_CTX));
-	if(ctx == nullptr) return -1;
-
-  	Blowfish_Init(ctx, (unsigned char*)"TESTKEY", 7);
-  	Blowfish_Encrypt(ctx, &L, &R);
-  	printf("%08X %08X\n", L, R);
-
-	if (L == 0xDF333FD2L && R == 0x30A71BB4L) printf("Test encryption OK.\n");
-  	else					  printf("Test encryption failed.\n");
-
-	Blowfish_Decrypt(ctx, &L, &R);
-
-  	if (L == 1 && R == 2) printf("Test decryption OK.\n");
-  	else 		      printf("Test decryption failed.\n");
-
-	Blowfish_Zero(ctx);
-	free(ctx);
-	*/
-
-	/*
-	// COMPRESS STRING AND EXPAND STRING TESTS
-	u32 comp[10];
-	int end = compressStr(argv[1], comp);
-
-	for(int i = 0; i < end; i++)
-	{
-		printf("%08X\n", comp[i]);
-	}
-
-	char exp[41];
-	expandStr(comp, 10, exp);
-
-	printf("%s\n", exp);
-
-	stripStr(exp);
-	printf("%s\n", exp);
-	*/
-
 	char input[256] = {0}, key[56] = {0}, *programName = argv[0];
 	int c;
 
@@ -109,7 +65,6 @@ int main(int argc, char *argv[])
 	                        // can use both single character and long options
 	                        // simultaniously, and the loop can test both.
 	                        if(strcmp((*argv) + 2, "help")    == 0) { showUsage(); exit(EXIT_SUCCESS);   }
-				if(strcmp((*argv) + 2, "hex") 	  == 0) { setbit(status, HEXMODE); continue; }
 				if(strcmp((*argv) + 2, "unhide")  == 0) { setbit(status, UNHIDE);  continue; }
 	                        if(strcmp((*argv) + 2, "ansi")    == 0) { setbit(status, ANSI);    continue; }
 	                }
@@ -120,7 +75,6 @@ int main(int argc, char *argv[])
 	                        {
 	                                case 'h': showUsage(); exit(EXIT_SUCCESS);
 					case 'u': setbit(status, UNHIDE);  break;
-					case 'x': setbit(status, HEXMODE); break;
 	                                case 'a': setbit(status, ANSI);    break;
 	                                // This error flag can either be set by a
 	                                // completely unrelated character inputted,
@@ -137,31 +91,78 @@ int main(int argc, char *argv[])
 	_Assert(input[0] != '\0', "no plaintext recieved");
 	_Assert(key[0] != '\0', "no key recieved");
 
-	printf("encrypting \"%s\" with key \"%s\"...\n", input, key);
+	printf("%s \"%s\" with key \"%s\"...\n", (test(status, UNHIDE)) ? "decrypting" : "encrypting", input, key);
 
-	// Compressing the given string into some length of 32-bit blocks
-	u32 compressedInput[128] = {0}, first = 0, second = 0;
-	int compLen = compressStr(input, compressedInput);
-
-	// Encryption!
+	// The P and S boxes need to be setup regardless of encryption or decryption
 	BLOWFISH_CTX *context = malloc(sizeof(BLOWFISH_CTX));
 	_Assert(context != NULL, "memory fault");
 
 	Blowfish_Init(context, (unsigned char *)key, strlen(key));
-	for(int i = 0; i < compLen; i += 2)
+
+	// Decrypting is a bit more complicated, sigh
+	if(test(status, UNHIDE))
 	{
-		first = compressedInput[i];
-		second = compressedInput[i + 1];
+		char buffer[5] = {0};
+		u32 first = 0, second = 0;
 
-		if(second == 0 || (i + 1) >= compLen) second = 0x9BADFACE;
+		// Decryption!
+		char *chunk = strtok(input, "-");
+		while(chunk != nullptr)
+		{
+			first =  (u32)(strtoul(chunk, nullptr, 16) >> 32);
+			second = (u32) strtoul(chunk, nullptr, 16);
 
-		Blowfish_Encrypt(context, &first, &second);
+			Blowfish_Decrypt(context, &first, &second);
 
-		if(test(status, HEXMODE)) 	printf("%X %X", first, second);
-		else 				printf("%u %u", first, second);
+			expand(first, buffer);
+			stripStr(buffer);
+			printf("%s", buffer);
+
+			// It's a bit hard to use stripStr() to remove
+			// the bad face, so for the moment, we can just
+			// stop ourselves from printing it
+			if(second != 0x9BADFACE)
+			{
+				expand(second, buffer);
+				stripStr(buffer);
+				printf("%s", buffer);
+			}
+
+			chunk = strtok(nullptr, "-");
+		}
+
+		putchar('\n');
 	}
+	// Encryption is a little more straightforward
+	else
+	{
+		// Compressing the given string into some length of 32-bit blocks
+		u32 compressedInput[128] = {0}, first = 0, second = 0;
+		int compLen = compressStr(input, compressedInput);
 
-	putchar('\n');
+		// Encryption!
+		for(int i = 0; i < compLen; i += 2)
+		{
+			first = compressedInput[i];
+			second = compressedInput[i + 1];
+
+			// Since Blowfish works in pairs, and I don't feel right
+			// simply appending a zero, I designated this constant
+			// to be appended in the case of the compression function
+			// above outputing an uneven amount of 32-bit blocks
+			if(second == 0 || (i + 1) >= compLen) second = 0x9BADFACE;
+
+			Blowfish_Encrypt(context, &first, &second);
+
+			// Every 64-bit block is segmented, so that if the output
+			// of this program were to be fed back into itself to decrypt,
+			// I would have an easier time extracting 32-bit chunks out of
+			// strings.
+			printf("%08X%08X-\n", first, second);
+		}
+
+		putchar('\n');
+	}
 
 	// Clean up
 	Blowfish_Zero(context);
--- README.md
+++ README.md
@@ -1,1 +1,58 @@
 # hide
+
+A simple Blowfish scramble schtick.
+First written by anson in late 2023-early 2024.
+
+This terminal uses the Blowfish block cipher
+originally written by Bruce Schneier in 1993.
+Although most of the code is written by anson,
+the specific C implementation of the Blowfish
+cipher used here was originally written by Paul
+Kocher in 1997.
+
+Command-line arguments are as follows:
+> hide (plain/ciphertext) (key) -a (--ansi) -u (--unhide) -h (--help)
+
+These arguments can be put in any order, alternated
+between their long and short forms, and their short
+forms can be globbed, such as "-au" which instructs
+hide to decrypt the following ciphertext with the
+given key, and produce ANSI colored output.
+
+Hide will detect any non-argument string passed to
+the function as plain/ciphertext, with the second
+non-argument string detected read as the key. Any
+additional input is ignored. As such, the arguments
+and input strings can be sent by the user in any
+arbitrary order.
+
+For more information regarding the Blowfish block
+cipher, see:
+www.schneier.com/academic/blowfish/
+
+For the original C implementation that this program
+was interpolated from, see:
+www.schneier.com/wp-content/uploads/2015/12/bfsh-koc.zip
+
+## Warning!
+
+I am not a cryptologist, only average enthusiast.
+I put together this program because I thought it
+would be fun to make, and although it uses a well-
+respected block cipher, I would only trust this
+program for trivial and low-stake matters. *I would
+not trust this program with sensitive information.*
+Please use this program with sensible input for
+sensible purposes.
+
+As of this program's current version (v.1.0.0, Jan 2024),
+the notice above still stands. I may consider improving
+this program to where it becomes more trustable, but
+as for now, use caution.
+
+### v.1.0.0 (First release)
+
+(Jan 2024)
+A simple Blowfish scramble schtick by anson.
+Be warned: the only cipher mode supported as of now is EBC.
+Use this application with the right data for the right purpose.
